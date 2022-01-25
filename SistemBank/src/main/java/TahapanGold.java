8a5e04c865be3cf0469349f4e2d2b3f06f51abed
Adi Firmansyah <38484738+firmanadi12@users.noreply.github.com>
2022-01-25 14:38:51 +0700

    Update TahapanGold.java

--- SistemBank/src/main/java/TahapanGold.java
+++ SistemBank/src/main/java/TahapanGold.java
@@ -4,13 +4,13 @@ public class TahapanGold extends Rekening {
     
     TahapanGold(double jumlahDeposit){
         this.setSaldo(jumlahDeposit);
-        this.setBunga(0.05);   
+
     }
     @Override
     public String toString(){
        return "Tipe Rekening : "+tipeRekening+"\n"+
                "Nomor Rekening : "+this.getNomorRekening()+"\n"+
                "Saldo : "+this.getSaldo()+"\n"+
-               "Bunga : "+this.getBunga()+"%\n";
+
     }
 }
